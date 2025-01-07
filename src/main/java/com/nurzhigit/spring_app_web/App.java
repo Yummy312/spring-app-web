d790b917e102f6c39c271bd431bf82575eee7821
Yummy312 <kurmushuev.nurzhigit@gmail.com>
2025-01-07 18:23:18 +0600

    Bean Scope Singleton

--- src/main/java/com/nurzhigit/spring_app_web/App.java
+++ src/main/java/com/nurzhigit/spring_app_web/App.java
@@ -7,10 +7,14 @@ public class App {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("ApplicationContext.xml");
 
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusic();
-        System.out.println(musicPlayer.getName());
-        System.out.println(musicPlayer.getVolume());
+        MusicPlayer firstmusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer secondmusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+
+
+        System.out.println(firstmusicPlayer.toString()); // com.nurzhigit.spring_app_web.MusicPlayer@4988d8b8
+        System.out.println(secondmusicPlayer.toString()); // com.nurzhigit.spring_app_web.MusicPlayer@4988d8b8
+        // В Spring бины по умолчанию создаются с областью видимости (scope) как singleton, что означает,
+        // что для каждого бина создается только один экземпляр на весь контекст приложения.
 
         context.close();
     };
