d93ce4281657f5add3e81990457b9383b7ec4126
Yummy312 <kurmushuev.nurzhigit@gmail.com>
2025-01-08 18:10:31 +0600

    Аннотация @Autowired. Внедрение зависимостей(Dependency Injecton), внедрение через конструктор класса

--- src/main/java/com/nurzhigit/spring_app_web/App.java
+++ src/main/java/com/nurzhigit/spring_app_web/App.java
@@ -7,17 +7,9 @@ public class App {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("ApplicationContext.xml");
 
-        ClassicalMusic classicalMusic = context.getBean("classicalMusic", ClassicalMusic.class);
-        System.out.println(classicalMusic.getSong()); // Hungarian Rhapsody
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        musicPlayer.playMusic();
 
-        MusicPlayer musicPlayer = new MusicPlayer(classicalMusic);
-        musicPlayer.playMusic(); // Playing: Hungarian Rhapsody
-
-        RockMusic rockMusic = context.getBean("rockMusic", RockMusic.class);
-        System.out.println(rockMusic.getSong());
-
-        MusicPlayer musicPlayer2 = new MusicPlayer(rockMusic);
-        musicPlayer2.playMusic();
 
 
         context.close();
--- src/main/java/com/nurzhigit/spring_app_web/MusicPlayer.java
+++ src/main/java/com/nurzhigit/spring_app_web/MusicPlayer.java
@@ -1,39 +1,17 @@
 package com.nurzhigit.spring_app_web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer {
-    private Music music;
-    private String name;
-    private int volume;
+    private ClassicalMusic classicalmusic;
 
-    public String getName() {
-        return name;
+    @Autowired
+    public MusicPlayer (ClassicalMusic classicalmusic) {
+        this.classicalmusic = classicalmusic;
     }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getVolume() {
-        return volume;
-    }
-
-    public void setVolume(int volume) {
-        this.volume = volume;
-    }
-
-    public  MusicPlayer(Music music){
-        this.music = music;
-    };
-    public MusicPlayer(){}
-    public void setMusic(Music music){
-        this.music = music;
-    }
-
-
     public void playMusic(){
-        System.out.println("Playing: " + music.getSong());
+        System.out.println("Playing: " + classicalmusic.getSong());
     }
 }
