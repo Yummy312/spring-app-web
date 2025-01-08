e3463ef47f452b2c2c18bcc0bcf2022218df95f7
Yummy312 <kurmushuev.nurzhigit@gmail.com>
2025-01-08 19:57:02 +0600

    Аннотация @Qualifier. Внедрение зависимостей(Dependency Injecton), внедрение через поле

--- src/main/java/com/nurzhigit/spring_app_web/MusicPlayer.java
+++ src/main/java/com/nurzhigit/spring_app_web/MusicPlayer.java
@@ -1,16 +1,19 @@
 package com.nurzhigit.spring_app_web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer {
-
     @Autowired
-    private ClassicalMusic classicalmusic;
+    @Qualifier("classicalMusic")
+    private Music music;
+
 
     public void playMusic(){
-        System.out.println("Playing: " + classicalmusic.getSong());
+        System.out.println("Playing: " + music.getSong());
+
     }
 
 
